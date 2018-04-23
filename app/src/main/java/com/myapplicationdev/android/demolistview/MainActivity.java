1c2e4287453c928f8fce55d06a196a2a6267f8c7
AnjanaGeorge <16004097@myrp.edu.sg>
2018-04-23 11:45:07 +0800

    until adapter

--- app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
@@ -1,13 +1,55 @@
 package com.myapplicationdev.android.demolistview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Food> food;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) this.findViewById(R.id.lvFood);
+
+        food = new ArrayList<Food>();
+        food.add(new Food("Ah Lat Coffee", false));
+        food.add(new Food("Rocky Choc", true));
+        food.add(new Food("Kid Cat Choc", true));
+
+        aa = new FoodAdapter(this, R.layout.row, food);
+        lv.setAdapter(aa);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Food selectedFood = food.get(position);
+
+                Toast.makeText(MainActivity.this, selectedFood.getName()
+                                + " Star: " + selectedFood.isStar(),
+                        Toast.LENGTH_LONG).show();
+                String a = selectedFood.isStar()+"";
+                String[] info = {selectedFood.getName(),a};
+                Intent intent = new Intent(getBaseContext(),NewActivity.class);
+                intent.putExtra("item",info);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
